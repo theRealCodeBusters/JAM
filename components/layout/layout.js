cad9a011f724ac2dbac7efcb51fad9409e0f152e
Markus-js <61780685+Markus-js@users.noreply.github.com>
2022-07-12 10:45:38 +0200

    layout ui

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -3,13 +3,12 @@ import Header from "./header"
 import Footer from "./footer"
 
 
-
 export default function Layout({ children }) {
   return (
-    <>
+    <div className="layout">
       <Header />
       <main>{children}</main>
       <Footer />
-    </>
+    </div>
   )
 }
