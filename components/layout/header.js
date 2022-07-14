9a2a2551f6cc73a8793a5ce177fba0b3059705a3
Markus-js <61780685+Markus-js@users.noreply.github.com>
2022-07-14 12:31:07 +0200

    update exports from header

--- components/layout/header.js
+++ components/layout/header.js
@@ -1,19 +1,17 @@
 import React from 'react'
 import Link from "next/link"
 
-const header = () => {
+export default function Header() {
   return (
     <header className='header'>
       <nav className="navbar">
         <h2 className="navbar__logo">CodeBusters</h2>
         <ul className='nav-links'>
           <Link href='/'><h3><li className='nav-links__link'>Home</li></h3></Link>
-          <Link  href='/products'><h3><li className='nav-links__link'>Products</li></h3></Link>
+          <Link href='/products'><h3><li className='nav-links__link'>Products</li></h3></Link>
         </ul>
         <h1 className='navbar__title'>JAMify</h1>
       </nav>
     </header>
   )
 }
-
-export default header
